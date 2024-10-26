5221cc517095adc38b0b8e27f3ef847dccfbb284
Ari Setiawan <ari_22510010@stimata.ac.id>
2024-10-26 19:21:20 +0700

    expo

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -29,15 +29,15 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'HOME',
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
               <Pressable>
                 {({ pressed }) => (
                   <FontAwesome
                     name="info-circle"
-                    size={25}
+                    size={20}
                     color={Colors[colorScheme ?? 'light'].text}
                     style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
                   />
@@ -50,8 +50,15 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'BERITA',
+          tabBarIcon: ({ color }) => <TabBarIcon name="book" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'PROFILE',
+          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,36 @@
-import { StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-
-export default function TabOneScreen() {
+export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <View style={styles.header}>
+        <Text style={styles.title}>Halo,</Text>
+        <Text style={styles.description}>Ari Setiawan</Text>
+        <Text style={styles.NIM}>( 22.51.0010 )</Text>
+      </View>
+      <View style={styles.iconContainer}>
+        <View style={styles.iconItem}>
+          <Icon name="assignment" size={45} color="black" />
+          <Text style={styles.iconLabel}>KRS</Text>
+        </View>
+        <View style={styles.iconItem}>
+          <Icon name="grade" size={45} color="black" />
+          <Text style={styles.iconLabel}>Nilai</Text>
+        </View>
+        <View style={styles.iconItem}>
+          <Icon name="check-circle" size={45} color="black" />
+          <Text style={styles.iconLabel}>Presensi</Text>
+        </View>
+        <View style={styles.iconItem}>
+          <Icon name="inbox" size={45} color="black" />
+          <Text style={styles.iconLabel} />
+        </View>
+      </View>
+      <View style={styles.scheduleContainer}>
+        <Text style={styles.scheduleTitle}>Jadwal Kuliah</Text>
+        <Text style={styles.scheduleText}>Jadwal kuliah tidak tersedia</Text>
+      </View>
     </View>
   );
 }
@@ -16,16 +38,62 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 15,
+  },
+  header: {
+    paddingVertical: 10,
+    backgroundColor: 'white',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: 'blue',
+    marginBottom: 10,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  description: {
+    fontSize: 36,
+    textAlign: 'left',
+    fontWeight : 'bold',
+    marginBottom: 10,
+  },
+  NIM: {
+    fontSize: 24,
+    textAlign: 'left',
+    marginBottom: 10,
+    color: 'blue',
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginTop: 20,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 10,
+    padding: 35,
+  },
+  iconItem: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  iconLabel: {
+    marginTop: 5,
+    fontSize: 18,
+    color: 'black',
+  },
+  scheduleContainer: {
+    marginTop: 30,
+    alignItems: 'flex-start',
+  },
+  scheduleTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'black',
+    marginBottom: 15,
+  },
+  scheduleText: {
+    fontSize: 20,
+    color: 'gray',
   },
 });
